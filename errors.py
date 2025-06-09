10d176c8a1aa218030116bcb9c9c431a4c58469a
DevGege <geovaneparedes2@gmail.com>
2025-06-09 15:20:37 -0300

    "Erro: LBYL + race condition"

--- errors.py
+++ errors.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
-names = open("names.txt").readlines()
-print(names[2])
+
+import sys
+import os
+
+if os.path.exists("names.txt"):
+    input(...) #Race condition to check if the file exists
+    names = open("names.txt").readlines()
+if len(names) >= 3:
+    print("Error: The file does not contain enough names.",names[2])
+else:
+    print("Error: The file 'names.txt' does not exist.")
+    sys.exit(1)
+if len(names) >= 3:
+    print(names[2])
+else:
+    print("[Error]. Missing name in the list.")
+    sys.exit(1)
