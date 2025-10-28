5566c205ba56c4a4679974091b4849cf2b0b3a41
GeovaneParedes <geovaneparedes@gmail.com>
2025-10-28 15:51:43 -0300

    Fazendo Recrusao de Erros em Python

--- errors.py
+++ errors.py
@@ -3,22 +3,26 @@
 import sys
 import os
 import logging
+import time
 
 log = logging.Logger("erros")
 # EAFP - Easy to ASK Forgiveness than permission
 # (E mais facil pedir perdao do que permisao)
 def try_to_open_a_file(file_path, retry=1) -> list:
-    for attempt in range(1, retry + 1):
-        try:
-            return open(file_path).readlines()
-        except FileNotFoundError as e:
-            log.error("ERROR: %s", e)
-        else:
-            print("Sucesso!!!")
-            return file
-        finally:
-            print("Executando o finally")
+    if retry > 999:
+        raise ValueError("Numero de tentativas muito alto")
+    try:
+        return open(file_path).readlines()
+    except FileNotFoundError as e:
+        log.error("ERROR: %s", e)
+        time.sleep(2)
+        if retry > 1:
+            return try_to_open_a_file(file_path, retry=retry - 1)
+    else:
+        print("Sucesso!!!")
+    finally:
+        print("Executando o finally")
     return []
 
-for line in try_to_open_a_file("names.txt"):
+for line in try_to_open_a_file("names.txt", retry=5):
     print(line)
