781ad81b83b769bda9b11761222d01ca2dbacddd
GeovaneParedes <geovaneparedes@gmail.com>
2025-10-29 09:31:38 -0300

    Trabalhando repe

--- hello_v2.py
+++ hello_v2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """
 Hello World Multi Linguas
 
@@ -34,6 +33,13 @@ ch.setFormatter(fmt)
 log.addHandler(ch)
 
 arguments = {"lang": None,"count": 1}
+msg = {
+    "en_US": "Hello, World!",
+    "pt_BR": "Olá, Mundo!",
+    "it_IT": "Ciao, Mondo!",
+    "es_SP": "Hola, Mundo!",
+    "fr-FR": "Bonjour, Monde!"
+}
 
 for arg in sys.argv[1:]:
     try:
@@ -54,32 +60,24 @@ for arg in sys.argv[1:]:
     arguments[key] = value
 
 current_language = arguments["lang"]
-if current_language is None:
-    if "LANG" in os.environ:
-        current_language = os.getenv("LANG")
-    #TODO: Usar repeticao
-    else:
-        current_language = input("Choose a Language:")
-current_language = current_language[:5]
+while True:
 
-msg = {
-    "en_US": "Hello, World!",
-    "pt_BR": "Olá, Mundo!",
-    "it_IT": "Ciao, Mondo!",
-    "es_SP": "Hola, Mundo!",
-    "fr-FR": "Bonjour, Monde!"
-}
-# LBYL
-
-try:
-    message = msg[current_language]
-except KeyError as e:
-    print(f"[ERROR] {str(e)}")
-    print(f"Language is Invalid, choose from: {list(msg.keys())}")
-    sys.exit(1)
-print(message * int(arguments["count"]))
- 
-print('My name is Geovane Paredes')
-print('I am a software engineer Python developer')
-print('I am developing python jr')
-print('I am learning how to use Git and GitHub')
+    if current_language is None:
+        if "LANG" in os.environ:
+            current_language = os.getenv("LANG")
+        else:
+            current_language = input("Choose a Language:")
+    current_language = current_language[:5]
+    try:
+        message = msg[current_language]
+    except KeyError as e:
+        print(f"[ERROR] {str(e)}")
+        print(f"Language is Invalid, choose from: {list(msg.keys())}")
+        sys.exit(1)
+    print(message * int(arguments["count"]))
+    
+    print('My name is Geovane Paredes')
+    print('I am a software engineer Python developer')
+    print('I am developing python jr')
+    print('I am learning how to use Git and GitHub')
+    break
