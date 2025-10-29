d241ee82096b7c7592a354a05b5ff67f1721e1e1
GeovaneParedes <geovaneparedes@gmail.com>
2025-10-29 09:42:28 -0300

    Trabalhando em hello_v2.py com Repeticao While Loop

--- hello_v2.py
+++ hello_v2.py
@@ -18,27 +18,23 @@ import sys
 import logging
 
 log_level = os.getenv("LOG_LEVEL", "WARNING").upper()
-
 log = logging.Logger("Geovane", log_level)
-
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
-
 fmt = logging.Formatter(
     '%(asctime)s %(name)s %(levelname)s '
     'l:%(lineno)d f:%(filename)s: %(message)s'
 )
-
 ch.setFormatter(fmt)
 log.addHandler(ch)
 
-arguments = {"lang": None,"count": 1}
+arguments = {"lang": None, "count": 1}
 msg = {
     "en_US": "Hello, World!",
     "pt_BR": "Olá, Mundo!",
     "it_IT": "Ciao, Mondo!",
     "es_SP": "Hola, Mundo!",
-    "fr-FR": "Bonjour, Monde!"
+    "fr_FR": "Bonjour, Monde!"
 }
 
 for arg in sys.argv[1:]:
@@ -56,28 +52,35 @@ for arg in sys.argv[1:]:
     value = value.strip()
     if key not in arguments:
         print(f"Invalion Option `{key}`")
-        sys.exit()
+        sys.exit(1)
     arguments[key] = value
 
-current_language = arguments["lang"]
 while True:
-
+    current_language = arguments["lang"]
     if current_language is None:
         if "LANG" in os.environ:
             current_language = os.getenv("LANG")
         else:
-            current_language = input("Choose a Language:")
+            current_language = input("Choose a language (ex: pt_BR, en_US): ").strip()
+
     current_language = current_language[:5]
     try:
         message = msg[current_language]
-    except KeyError as e:
-        print(f"[ERROR] {str(e)}")
-        print(f"Language is Invalid, choose from: {list(msg.keys())}")
-        sys.exit(1)
-    print(message * int(arguments["count"]))
-    
-    print('My name is Geovane Paredes')
-    print('I am a software engineer Python developer')
-    print('I am developing python jr')
-    print('I am learning how to use Git and GitHub')
-    break
+    except KeyError:
+        print(f"[ERROR] Language '{current_language}' is invalid.")
+        print(f"Choose from: {list(msg.keys())}")
+        continue
+
+    count = arguments.get("count")
+    try:
+        count = int(count)
+    except (TypeError, ValueError):
+        count = 1
+
+    print(message * count)
+
+    resp = input("Do you want to continue? [y/N]: ")
+    if resp.lower() != 'y':
+        break
+
+    arguments["lang"] = input("Which language do you want the greeting in? (ex: pt_BR): ").strip()
